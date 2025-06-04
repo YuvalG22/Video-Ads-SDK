0f9974ec58f2cd1513db6792f4b8677ffcbc1e25
Yuval Ganon <yuvalpay22@gmail.com>
2025-06-04 21:45:26 +0300

    fix gradle 1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ afterEvaluate {
 
                 groupId = "com.github.YuvalG22"
                 artifactId = "videoadslibrary"
-                version = "1.4"
+                version = "1.7"
             }
         }
     }
