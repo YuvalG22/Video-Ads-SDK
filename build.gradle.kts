ea3d902dc6f0acc83d077e0a232a7f0a4c891867
Yuval Ganon <yuvalpay22@gmail.com>
2025-06-04 22:33:17 +0300

    fix gradle 2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "8.9.2"
+    id("com.android.library")
     id("maven-publish")
 }
 
@@ -59,7 +59,7 @@ afterEvaluate {
 
                 groupId = "com.github.YuvalG22"
                 artifactId = "videoadslibrary"
-                version = "1.7"
+                version = "2.2"
             }
         }
     }
